8891bb69ade0a26c325ee94e5dbdaf9640e4eb0d
root <root@ip-172-31-21-35.ec2.internal>
2026-02-21 04:24:00 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("Node App Deployed via Jenkins + Docker 🚀");
+});
+
+app.listen(3000, () => console.log("Server started on 3000"));
